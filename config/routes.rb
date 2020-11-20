e3ec03ab800836033197db8fc044964d186c963c
EC2 Default User <ec2-user@ip-172-31-37-182.ec2.internal>
2020-11-20 13:06:14 +0000

    [add]フォローボタン

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :users, only: [:show,:index,:edit,:update]
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
